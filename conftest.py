36ce742c9cbad80cce9b0a3a6df89eaf24495f72
samuraii <konflic@mail.ru>
2020-07-26 12:51:26 +0300

    Версия 4.0

--- conftest.py
+++ conftest.py
@@ -1,46 +1,33 @@
 import pytest
-import os
 
 from selenium import webdriver
 
 
-def driver_factory(browser):
-    if browser == "chrome":
-        driver = webdriver.Chrome(executable_path="drivers/chromedriver")
-    elif browser == "firefox":
-        driver = webdriver.Firefox(executable_path="drivers/geckodriver")
-    elif browser == "yandex":
-        driver = webdriver.Chrome(executable_path="drivers/yandexdriver")
-    elif browser == "opera":
-        driver = webdriver.Opera(executable_path="drivers/operadriver")
-    elif browser == "safari":
-        driver = webdriver.Safari()
-    else:
-        raise Exception("Driver not supported")
-    return driver
-
-
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="chrome")
-    parser.addoption("--url", action="store", default="https://demo.opencart.com/", help="choose your browser")
+    parser.addoption("--browser", action="store", default="opera")
+    parser.addoption("--url", action="store", default="https://demo.opencart.com/")
 
 
 @pytest.fixture
 def browser(request):
+    # Сбор параметров запуска для pytest
     browser = request.config.getoption("--browser")
     url = request.config.getoption("--url")
-    driver = driver_factory(browser)
+    # Инициализация нужного объекта
+    drivers = "/Users/mikhail/Downloads/drivers"
+    if browser == "chrome":
+        driver = webdriver.Chrome(executable_path=drivers + "/chromedriver")
+    elif browser == "firefox":
+        driver = webdriver.Firefox(executable_path=drivers + "/geckodriver")
+    elif browser == "opera":
+        driver = webdriver.Opera(executable_path=drivers + "/operadriver")
+    else:
+        driver = webdriver.Safari()
+    # Предварительная настройка запуска
     driver.maximize_window()
-    driver.implicitly_wait(5)
     request.addfinalizer(driver.close)
     driver.get(url)
-    return driver
-
-
-@pytest.fixture(params=["chrome", "firefox", "opera"])
-def parametrize_browser(request):
-    driver = driver_factory(request.param)
-    driver.implicitly_wait(5)
-    request.addfinalizer(driver.quit)
-    driver.get(request.config.getoption("--url"))
+    # Сохраняю ссылку на базовый url
+    driver.url = url
+    # Выдача драйвера из фикстуры
     return driver
